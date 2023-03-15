08aa712fc27395c705add18295e6b3226e74151b
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-03-16 11:33:07 +1300

    Tidied frontend
    removed primevue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,38 +1,12 @@
 <script setup>
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import ColumnGroup from 'primevue/columngroup';   // optional
-import Row from 'primevue/row';                   // optional
+
 </script>
+
 <template>
 TEST FRONTEND
-<h1>{{ single_user.username }}</h1>
-<h1 @click="create_user">[ADD NEW USER]</h1>
-<div>
-  <label>username:</label>
-  <input type="text" placeholder="enter username" v-model="body_data.username">
-</div>
-<div>
-  <label>firstname:</label>
-  <input type="text" placeholder="enter firstname" v-model="body_data.fname">
-</div>
-<div>
-  <label>lastname:</label>
-  <input type="text" placeholder="enter lastname" v-model="body_data.lname">
-</div>
-{{ body_data }}
-<h3 v-for="user in users_list">
-  <span @click="fetch_single_user(user._id)">{{ user.username }}</span>
-  <span @click="delete_user(user._id)">[DELETE]</span>
-  <span @click="update_user(user._id)">[UPDATE]</span>
-</h3>
-<!-- Projects -->
-<DataTable :value="products" tableStyle="min-width: 50rem">
-    <Column field="code" header="Code"></Column>
-    <Column field="name" header="Name"></Column>
-    <Column field="category" header="Category"></Column>
-    <Column field="quantity" header="Quantity"></Column>
-</DataTable>
+<!-- division to show all projects fetched -->
+<!-- UI element to trigger the menu to post new projects -->
+<!-- UI element on page to post new projects -->
 </template>
 <style scoped>
 </style>
@@ -40,52 +14,52 @@ TEST FRONTEND
 export default {
   data(){
     return{
-      users_list:[],
-      single_user:{username:'',fname:'',lname:''},
-      body_data:{username:'',fname:'',lname:''}
+      projects_list:[],
+      single_project:{projectName:'',author:'',imageURL:'',projectURL:''},
+      body_data:{projectName:'',author:'',imageURL:'',projectURL:''}
     }
   },
   methods:{
-    async fetch_all_users(){
-      const response = await fetch("http://localhost:4000/users/");
+    async fetch_all_projects(){
+      const response = await fetch("http://localhost:4000/projects/");
       const received_data = await response.json();
-      this.users_list=received_data;
+      this.projects_list=received_data;
     },
-    async fetch_single_user(userID){
-      const response = await fetch("http://localhost:4000/users/getuser/"+userID);
+    async fetch_single_project(projectID){
+      const response = await fetch("http://localhost:4000/projects/get/"+projectID);
       const received_data = await response.json();
-      this.single_user=received_data;
+      this.single_project=received_data;
     },
-    async delete_user(userID){
-      let fetch_url = 'http://localhost:4000/users/delete/'+userID;
+    async delete_project(projectID){
+      let fetch_url = 'http://localhost:4000/projects/delete/'+projectID;
       const response = await fetch(fetch_url, {method:"DELETE"});
       const received_data = await response.json();
-      this.fetch_all_users();
+      this.fetch_all_projects();
     },
-    async update_user(userID){
-      const response = await fetch("http://localhost:4000/users/update/"+userID, {
+    async update_project(projectID){
+      const response = await fetch("http://localhost:4000/projects/update/"+projectID, {
         method:"PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(this.body_data)
       });
       const received_data = await response.json();
-      this.fetch_all_users();
+      this.fetch_all_projects();
     },
-    async create_user(){
-      const response = await fetch("http://localhost:4000/users/adduser", {
+    async create_new_project(){
+      const response = await fetch("http://localhost:4000/projects/addproject", {
         method:"POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(this.body_data)
       });
       const received_data = await response.json();
-      this.fetch_all_users();
+      this.fetch_all_projects();
     },
     async fetch_sample(){
       const response = await fetch( url, { /* info about fetch */ });
     }
   },
   created(){
-    this.fetch_all_users();
+    this.fetch_all_projects();
   }
 }
 </script>
