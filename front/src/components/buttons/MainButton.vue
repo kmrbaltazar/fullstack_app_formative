ff38b1d0b22cb829de6736b0ca6b703396061645
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-03-16 12:48:34 +1300

    Created button components

---
+++ front/src/components/buttons/MainButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="button-wrapper">
+    <h5>{{ main_btn_prop }}</h5>
+  </div>
+</template>
+
+<style scoped>
+.button-wrapper {
+    padding: 7px 24px;
+    background-color: #D6741B;
+    color: #fff;
+    height: 35px;
+    width: 8rem;
+    border-radius: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.button-wrapper:hover {
+    background-color: #AB5201;
+}
+</style>
+
+<script setup>
+defineProps({
+  main_btn_prop: {
+    type: String,
+    required: true
+  }
+});
+</script>
