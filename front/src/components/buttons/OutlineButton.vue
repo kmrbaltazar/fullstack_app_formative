5818afe8aa01a962ac8fbc9d4b64c51e4c1b1dc3
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-03-16 14:17:28 +1300

    Styled Outline Button

--- front/src/components/buttons/OutlineButton.vue
+++ front/src/components/buttons/OutlineButton.vue
@@ -1,10 +1,36 @@
 <template>
-    
+  <div class="button-wrapper">
+    <h5>{{ outline_btn_prop }}</h5>
+  </div>
 </template>
 
 <style scoped>
+.button-wrapper {
+  padding: 7px 24px;
+  background-color: #fff;
+  color: #d6741b;
+  height: 35px;
+  width: 8rem;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #d6741b;
+}
 
-
+.button-wrapper:hover {
+  color: #ab5201;
+  border: 2px solid #ab5201;
+  background-color: #fff;
+  filter: drop-shadow(2px 4px 4px rgba(0, 0, 0, 0.25));
+}
 </style>
 
-<script></script>
+<script setup>
+defineProps({
+  outline_btn_prop: {
+    type: String,
+    required: true,
+  },
+});
+</script>
